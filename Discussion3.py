94835991b67f22fb846f76e2e466bca7efd91845
Jake Verbick <jakeverbick@Jakes-MacBook-Pro-2.local>
2026-01-28 11:44:09 -0500

    first commit

--- Discussion3.py
+++ Discussion3.py
@@ -1,19 +1,39 @@
 import math
-
+"""Problem 1. Create the constructor "__init__" method
+with arguments width (an integer), height (an integer)
+(1) It sets an instance variable, "width" to the passed argument, width
+(2) It sets an instance variable, "height" to the passed argument, height
+Problem 2. Create the "__str__" method
+It returns a string, "A rectangle with width <width> and height
+<height>"
+for example, "A rectangle with width 3 and height 6
+Problem 3. Create the "area_calculator" method
+It returns the area of the rectangle (float)
+Area of rectangle = length × width.
+Problem 4. Create the "__eq__" method with two
+arguments: self and other (an object)
+It returns a boolean
+True if the two rectangles have the same width
+and the same height
+False otherwise
+"""
 class Rectangle():
     # Create the constructor "__init__" method
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
     # YOUR CODE HERE
 
 
 
     # Create the "__str__" method
-
-    # YOUR CODE HERE
-
-
+    def __str__(self):
+        return f"A rectangle with width {self.width} and height {self.height}"
 
     # Create the "area_calculator" method
+    def area_calculator(height, width):
+        
 
     # YOUR CODE HERE
 
@@ -27,26 +47,26 @@ class Rectangle():
 
 
     
-
-
-def main():
-    r1 = Rectangle(10, 10)
+    
+    
+    def main():
+        r1 = Rectangle(10, 10)
     # call the __str__ method
-    print(r1)
+        print(r1)
     # call the area_calculator method
-    print("Area:", r1.area_calculator())
+        print("Area:", r1.area_calculator())
 
 
-    r2 = Rectangle(10, 15)
-    print(r2)
-    print("Area:", r2.area_calculator())
+        r2 = Rectangle(10, 15)
+        print(r2)
+        print("Area:", r2.area_calculator())
     # call the __eq__ method
-    print(r1 == r2)
-    print()
+        print(r1 == r2)
+        print()
 
     # you can create additional rectangle objects to 
     # test your code or learn more about how the class behaves
-    pass
+        pass
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
